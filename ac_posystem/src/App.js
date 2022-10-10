2604d06759660f3e2eec5e24d058bbf796a9f9ce
“AkhilPJ12” <akhilpj12@xn--gmail-hw3b.com>
2022-10-10 09:51:52 -0500

    Added all input variables in the payment page without css

--- ac_posystem/src/App.js
+++ ac_posystem/src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn ReactJS 
-        </a>
-      </header>
-    </div>
+
+    <div class="container">
+
+    <form action="">
+
+        <div class="row">
+
+            <div class="col">
+
+                <h3 class="title">Billing address</h3>
+
+                <div class="inputBox">
+                <label>Enter your name:<input type="text" /></label>
+                </div>
+                <div class="inputBox">
+                <label>Enter your Email:<input type="email" /></label>
+                </div>
+
+                <div class="inputBox">
+                <label>Enter your Address:<input type="text" /></label>
+                </div>
+
+                <div class="inputBox">
+                <label>Enter your City:<input type="text" placeholder="mumbai"/></label>
+                </div>
+
+                <div class="flex">
+                    <div class="inputBox">
+                    <label>Enter your state:<input type="text" placeholder="AP"/></label>
+                    </div>
+                    <div class="inputBox">
+                    <label>Enter your zip code:<input type="text" /></label>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col">
+
+                <h3 class="title">payment</h3>
+
+                <div class="inputBox">
+                    <span>cards accepted :</span>
+                    
+                </div>
+                <div class="inputBox">
+                    <label>name on card:<input type="text" /></label>
+                </div>
+                <div class="inputBox">
+                <label>credit card number:<input type="number" /></label>
+                </div>
+
+                <div class="inputBox">
+                <label>exp month:<input type="text" /></label>
+                </div>
+
+                <div class="flex">
+                    <div class="inputBox">
+                    <label>exp year:<input type="number" /></label>
+                    </div>
+
+                    <div class="inputBox">
+                    <label>CVV:<input type="text" /></label>
+                    </div>
+                </div>
+
+            </div>
+    
+        </div>
+        
+        <label>proceed to checkout :<input type="submit" /></label>
+
+    </form>
+
+</div>  
+
   );
 }
 
