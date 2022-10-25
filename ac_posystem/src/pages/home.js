ceea992018ca32a527e95231ce2631c3351ccffb
“AkhilPJ12” <akhilpj12@xn--gmail-hw3b.com>
2022-10-25 17:09:04 -0500

    Adding Navigations to the payment page

--- ac_posystem/src/pages/home.js
+++ ac_posystem/src/pages/home.js
@@ -3,63 +3,69 @@ import React from 'react'
 class Home extends React.Component{
     render(){
         return(
-        <div>
-            <div class="container">
-
+    <div>
+      <ul>
+	  <li><a class="back" href="#back">Back</a></li>
+	  <li><a class="active" href="#home">Home</a></li>
+	  <li><a href="#contact">Contact</a></li>
+	  <li style="float:right"><a href="#about">About</a></li>
+	</ul> 
+    <div className="container">
+    
     <form action="">
+   
+    <div className="row">
+    
+        <div className="col">
+       
+            <h3 className="title">BILLING ADDRESS (OPTINAL)</h3>
 
-    <div class="row">
-
-        <div class="col">
-
-            <h3 class="title">BILLING ADDRESS (OPTINAL)</h3>
-
-            <div class="inputBox">
+            <div className="inputBox">
                 <span>full name :</span>
                 <input type="text" placeholder="Sam" />
             </div>
-            <div class="inputBox">
+            <div className="inputBox">
                 <span>email :</span>
                 <input type="email" placeholder="Sam@example.com" />
             </div>
-            <div class="inputBox">
+            <div className="inputBox">
                 <span>Phone Number :</span>
                 <input type="text" placeholder="+1-1234567890" />
             </div>
 
             <div >
-                <div><img src="/asserts/images/cash.png" alt="Cash Icon" width="320" height="120" /></div>
-                <div><input type="submit" value="Cash" class="submit" /> </div>
+                <div><img src="/asserts/images/image3.jpeg" alt="Cash Icon" width="320" height="120" /></div>
+                <div><input type="submit" value="Cash" className="submit" /> </div>
             </div>
 
         </div>
 
-        <div class="col">
+        <div className="col">
 
-            <h3 class="title">payment</h3>
+            <h3 className="title">payment</h3>
 
-            <div class="inputBox">
+            <div className="inputBox">
                 <span>cards accepted :</span>
                 <img src="/asserts/images/card_img.png" alt="" />
             </div>
-            <div class="inputBox">
+            <div className="inputBox">
                 <span>name on card :</span>
                 <input type="text" placeholder="Mr. Sam" />
             </div>
-            <div class="inputBox">
+            <div className="inputBox">
                 <span>credit card number :</span>
                 <input type="number" placeholder="1111-2222-3333-4444" />
             </div>
-            <div class="inputBox">
+            <div className="inputBox">
                 <span>exp month :</span>
                 <input type="text" placeholder="january" />
             </div>
-            <div class="flex">
-                <div class="inputBox">
+            <div className="flex">
+                <div className="inputBox">
                     <span>exp year :</span>
                     <input type="number" placeholder="2022" />
                 </div>
-                <div class="inputBox">
+                <div className="inputBox">
                     <span>CVV :</span>
                     <input type="text" placeholder="123" />
                 </div>
@@ -69,11 +75,12 @@ class Home extends React.Component{
 
     </div>
 
-    <input type="submit" value="proceed to checkout" class="submit-btn" />
+    <input type="submit" value="proceed to checkout" className="submit-btn" />
 
 </form>
 
 </div> 
+
         </div>
         ); 
     }
