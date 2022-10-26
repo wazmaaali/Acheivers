f3b52091df638f19af3bc35d972c33ffc76e8e53
Madhavareddymukku <115957057+Madhavareddymukku@users.noreply.github.com>
2022-10-26 16:31:29 -0500

    Add files via upload
    Previous confirmation page was removed due to bugs and changes were made accordingly to make a new file.

---
+++ ac_posystem/src/pages/ConfiromationPageNew.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1 style={{textAlign: "center", fontFamily: "Times New Roman"}}><i>Thank you for Choosing our services</i></h1>
+      <div style={{border: "solid purple", borderRadius: "10px", margin: "10px", padding: "10px", backgroundImage: "linear-gradient(to right,#98ff98,#9ea8f5,#7cbcdc)"}}>The Total amount in the bill is:' '</div>
+      <h3 style={{textAlign: "center"}} >Please visit Again :-)</h3>
+    </div>
+  );
+}
+
+export default App;
