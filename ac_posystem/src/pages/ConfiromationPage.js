a55e5217fc3c489e8fc8c0692b0b3d6916165b82
Madhavareddymukku <115957057+Madhavareddymukku@users.noreply.github.com>
2022-10-25 22:12:20 -0500

    Displaying the final message
    This code displays the final message after purchasing and also displays the total amount of the bill.

---
+++ ac_posystem/src/pages/ConfiromationPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ConfiromationPage() {
+    return (
+    
+       <div>
+            
+    <h1 align="center" style="text-align: center; font-family: Times New Roman;"><i>Thank you for Choosing our services</i></h1>
+<div style="border: solid purple; border-radius: 10px; margin: 10px; padding: 10px;background-image: linear-gradient(to right,#98ff98,#9ea8f5,#7cbcdc);">The Total amount in the bill is:' '</div>
+    <h3 style="text-align: center;" >Please visit Again :-)</h3>
+ 
+       </div>
+);
+
+}
+
+
+export default ConfiromationPage;
