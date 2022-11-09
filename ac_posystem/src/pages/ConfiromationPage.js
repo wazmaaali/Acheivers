45c10c5c74f18a8db30863b3e09e7113d75fca8e
madhavareddymukku <115957057+Madhavareddymukku@users.noreply.github.com>
2022-11-09 11:28:32 -0600

    Branch

--- ac_posystem/src/pages/ConfiromationPage.js
+++ ac_posystem/src/pages/ConfiromationPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ConfiromationPage() {
   return (
     <div className="App">
-      
+      <head>Confirmation Page</head>
       <h1 style={{textAlign: "center", fontFamily: "Times New Roman"}}><i>Thank you for Choosing our services</i></h1>
       <div style={{border: "solid purple", borderRadius: "10px", margin: "10px", padding: "10px", backgroundImage: "linear-gradient(to right,#98ff98,#9ea8f5,#7cbcdc)"}}>The Total amount in the bill is:'z$23'</div>
       <h3 style={{textAlign: "center"}} >Please visit Again :-)</h3>
@@ -12,3 +12,4 @@ function ConfiromationPage() {
 }
 
 export default ConfiromationPage;
+// Done
