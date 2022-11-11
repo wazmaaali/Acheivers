00c1fb6be2fa5444d240ddf6de193d775698733e
“AkhilPJ12” <akhilpj12@xn--gmail-hw3b.com>
2022-11-11 14:15:42 -0600

    Test Cases

--- ac_posystem/src/pages/test/test.js
+++ ac_posystem/src/pages/test/test.js
@@ -10,3 +10,39 @@ describe("This suit is to test the Body component", ()=>{
         expect(getByText("Achievers Grocery")).toBeInTheDocument("Achievers Grocery")
     })
 })
+
+describe("Cart total", ()=>{
+   
+    test("Checking cart", ()=>{
+        const cart = [
+            { cart: "Apple", price: 500 },
+            { cart: "Mango", price: 50 },
+            { cart: "Melon", price: 60 },
+            { cart: "Berry", price: 30 },
+            { cart: "Jack", price: 200 },
+        ]
+        // Calculations.getSum(cart);
+        expect(Calculations.getSum(cart)).toEqual(840);
+    })
+})
+
+// describe("Cart total", ()=>{
+   
+//     test("Checking check", ()=>{
+//         const cart = [
+//             { cart: "Apple", price: 500 },
+//             { cart: "Mango", price: 50 },
+//             { cart: "Melon", price: 60 },
+//             { cart: "Berry", price: 30 },
+//             { cart: "Jack", price: 200 },
+//         ]
+//         expect(cart.length).toEqual(0);
+//     })
+// })
+
+// test('render the title of an application', () => { 
+//     test("Checking check", ()=>{
+//     const titleEl = screen.getByText(/src/MainCategory);
+//     expect(titleEl).toBeInTheDocument();
+//     })
+//   });
