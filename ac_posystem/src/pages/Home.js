d71dbcccae01e0da7026a55ff1e99bcbdaeabb67
“AkhilPJ12” <akhilpj12@xn--gmail-hw3b.com>
2022-11-26 20:35:24 -0600

    Code Changes

--- ac_posystem/src/pages/Home.js
+++ ac_posystem/src/pages/Home.js
@@ -10,7 +10,7 @@ import React from "react";
 
 const Home = () => {
   const [home, setCategories] = useState([]);
-   useEffect(() => {
+  useEffect(() => {
     const fetchCategories = async () => {
       try {
         const res = await axios.get("http://localhost:8803/categories");
@@ -37,29 +37,29 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App" >
 
-     <section className="header">
+      <section className="header">
         <a className="logo">
-            <i className="fas fa-shopping-bag icon"></i>
-            <h1 className="logoTitle">Achievers Grocery</h1>
+          <i className="fas fa-shopping-bag icon"></i>
+          <h1 className="logoTitle">Achievers Grocery</h1>
         </a>
         <form action="" className="searchForm">
-            <input type="search" name="search" id="search" className="searchBox" placeholder="Search here..." />
-            <label htmlFor="search" className="searchPointer">
-                <i className="fas fa-search icon"></i>
-            </label>
+          <input type="search" name="search" id="search" className="searchBox" placeholder="Search here..." />
+          <label htmlFor="search" className="searchPointer">
+            <i className="fas fa-search icon"></i>
+          </label>
         </form>
         <div className="mobileMenuHandler">
-            <i className="fas fa-bars icon"></i>
+          <i className="fas fa-bars icon"></i>
         </div>
-     </section>
+      </section>
 
       <div className="Categories">
 
         {/* {home.map((home) => ( */}
         <div className="Category">
-         <section id="category" className="category">
+          <section id="category" className="category" >
             <h2 className="sectionTitle">
               Shop By <span>Category</span>
             </h2>
@@ -74,93 +74,95 @@ const Home = () => {
                 </figure>
 
 
-           <div className="container">
-             <div className="box">
-               <div className="content">
-                 <h3 id="h1" className="title"></h3>
-                 <h4 className="subTitle">Upto 27.5% off</h4>
-               </div>
-               <figure className="figure">
-                 <img src={image1} alt="banner" className="img" />
-               </figure>
-                <Link to="/MainCategory" state={{id: "1",}} className="btn"> Show Now</Link>
-              </div>
+                <div className="container">
+                  <div className="box">
+                    <div className="content">
+                      <h3 id="h1" className="title"></h3>
+                      <h4 className="subTitle">Upto 27.5% off</h4>
+                    </div>
+                    <figure className="figure">
+                      <img src={image1} alt="banner" className="img" />
+                    </figure>
+                    <Link to="/MainCategory" state={{ id: "1", }} className="btn"> Show Now</Link>
+                  </div>
 
-              <div className="box">
-                <div className="content">
-                  <h3 id="h2" className="title"></h3>
-                  <h4 className="subTitle">Upto 50% off</h4>
+                  <div className="box">
+                    <div className="content">
+                      <h3 id="h2" className="title"></h3>
+                      <h4 className="subTitle">Upto 50% off</h4>
+                    </div>
+                    <figure className="figure">
+                      <img id="img2" alt="banner" className="img" />
+
+                    </figure>
+                    <Link to="/MainCategory" state={{ id: "2", }} className="btn" // your data array of objects
+                    >Shop Now
+                    </Link>
+                  </div>
+
+                  <div className="box">
+                    <div className="content">
+                      <h3 id="h3" className="title"></h3>
+                      <h4 className="subTitle">Upto 35% off</h4>
+                    </div>
+                    <figure className="figure">
+                      <img id="img3" alt="banner" className="img" />
+                    </figure>
+                    <Link to="/MainCategory" state={{ id: "3", }} className="btn"// your data array of objects
+                    >Shop Now
+                    </Link>
+                  </div>
                 </div>
-                <figure className="figure">
-                  <img id="img2" alt="banner" className="img" />
 
-                </figure>
-                <Link to="/MainCategory" state={{ id: "2",}} className="btn" // your data array of objects
-                 >Shop Now
-                </Link> 
-              </div>
+                <div className="container">
+                  <div className="box">
+                    <div className="content">
+                      <h3 id="h4" className="title"></h3>
+                      <h4 className="subTitle">Upto 27.5% off</h4>
+                    </div>
+                    <figure className="figure">
+                      <img id="img4" alt="banner" className="img" />
 
-              <div className="box">
-                <div className="content">
-                  <h3 id="h3" className="title"></h3>
-                  <h4 className="subTitle">Upto 35% off</h4>
+                    </figure>
+                    <Link to="/MainCategory" state={{ id: "4", }} className="btn" // your data array of objects 
+                    >Show Now
+                    </Link>
+                  </div>
+
+                  <div className="box">
+                    <div className="content">
+                      <h3 id="h5" className="title"></h3>
+                      <h4 className="subTitle">Upto 50% off</h4>
+                    </div>
+                    <figure className="figure">
+                      <img id="img5" alt="banner" className="img" />
+
+                    </figure>
+                    <Link to="/MainCategory" state={{ id: "5", }} className="btn" // your data array of objects
+                    >Shop Now
+                    </Link>
+                  </div>
+
+                  <div className="box">
+                    <div className="content">
+                      <h3 id="h6" className="title"></h3>
+                      <h4 className="subTitle">Upto 35% off</h4>
+                    </div>
+                    <figure className="figure">
+                      <img id="img6" alt="banner" className="img" />
+
+                    </figure>
+                    <Link to="/MainCategory" state={{ id: "6", }} className="btn" // your data array of objects  
+                    >Shop Now
+                    </Link>
+                  </div>
                 </div>
-                <figure className="figure">
-                  <img id="img3" alt="banner" className="img" />
-                </figure>
-                <Link to="/MainCategory" state={{id: "3",}} className="btn"// your data array of objects
-                 >Shop Now
-                </Link>
-              </div>
-           </div>
 
-           <div className="container">
-              <div className="box">
-                <div className="content">
-                  <h3 id="h4" className="title"></h3>
-                  <h4 className="subTitle">Upto 27.5% off</h4>
-                </div>
-                <figure className="figure">
-                  <img id="img4" alt="banner" className="img" />
-
-                </figure>
-                <Link to="/MainCategory" state={{id: "4",}} className="btn" // your data array of objects 
-                 >Show Now
-                </Link>
-              </div>
-
-              <div className="box">
-                <div className="content">
-                  <h3 id="h5" className="title"></h3>
-                  <h4 className="subTitle">Upto 50% off</h4>
-                </div>
-                <figure className="figure">
-                  <img id="img5" alt="banner" className="img" />
-
-                </figure>
-                <Link to="/MainCategory" state={{id: "5",}} className="btn" // your data array of objects
-                  >Shop Now
-                </Link>
-              </div>
-
-              <div className="box">
-                <div className="content">
-                  <h3 id="h6" className="title"></h3>
-                  <h4 className="subTitle">Upto 35% off</h4>
-                </div>
-                <figure className="figure">
-                  <img id="img6" alt="banner" className="img" />
-
-                </figure>
-                <Link to="/MainCategory" state={{id: "6",}} className="btn" // your data array of objects  
-                 >Shop Now
-                </Link>
               </div>
             </div>
-
-         </section>
+          </section>
         </div>
-     </div>
+      </div>
     </div>
   );
 };
