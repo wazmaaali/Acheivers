9d5335b44da52cb2e5bc2850f14ec2cb27659a52
Madhavareddymukku <115957057+Madhavareddymukku@users.noreply.github.com>
2022-11-30 16:40:39 -0600

    Menu Category Update
    - Written complete React based category handler
    - Created event written value to target

--- ac_posystem/src/pages/MenuButton.js
+++ ac_posystem/src/pages/MenuButton.js
@@ -1,1 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+class Menu extends React.Component {
+    constructor() {
+      super();
+      this.handleChange = this.handleChange.bind(this);
+      this.state = {
+        selectValue:'Go To'
+      };
+    }
+    handleChange(event){
+      this.setState({selectValue:event.target.value});
+      <Link
+        to="/MainCategory"
+        state={{
+        id: "1",
+        }}
+    >
+        event.target.value
+    </Link>
+    }
+      
+    render() {
+      return (
+              
+      );
+    }
+  }
+  
+  export default Menu;
+
 
