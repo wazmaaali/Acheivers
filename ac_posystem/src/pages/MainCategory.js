8a0ffb78f5941565d05cb56a969a81b2d3de3b7a
Madhavareddymukku <115957057+Madhavareddymukku@users.noreply.github.com>
2022-12-01 14:43:00 -0600

    Updated Menu in Min category page

--- ac_posystem/src/pages/MainCategory.js
+++ ac_posystem/src/pages/MainCategory.js
@@ -3,75 +3,39 @@ import "../assets/css/fontawesome.min.css";
 import "../assets/css/default.css";
 import "../assets/css/style.css";
 import "../assets/css/responsive.css";
+import image1 from "../assets/images/product-1.png";
+import image2 from "../assets/images/product-2.png";
+import image3 from "../assets/images/product-3.png";
+import image4 from "../assets/images/product-4.png";
+import image5 from "../assets/images/product-5.png";
+import image6 from "../assets/images/product-6.png";
+import image7 from "../assets/images/product-7.png";
+import image8 from "../assets/images/product-8.png";
+import image9 from "../assets/images/product-9.png";
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+
 import { Link } from "react-router-dom";
 
 var cat = "";
 var datalist = [];
 var addTOCart = [];
-
 const MainCategory = () => {
   const location = useLocation();
   cat = location.state.id;
-  console.log("9999 cat_id: ", cat);
+  const catt = JSON.parse(sessionStorage.getItem("cat"));
 
-  const [counter, setCount] = useState(0);
-
-  let incrementCount = (e) => {
-    setCount(counter + 1);
-    //jquery for showing popup
-    const modal = document.querySelector(".modal");
-    const closeBtn = document.querySelector(".close");
-    modal.style.display = "block";
-    closeBtn.addEventListener("click", () => {
-      modal.style.display = "none";
-    });
-
-    const mapping = { b1: 0, b2: 1, b3: 2, b4: 3, b5: 4, b6: 5 };
-    const a = e.target.id;
-    const index = mapping[a];
-    console.log("INDEX == ", index);
-    var item = addTOCart.find((x) => x.sc_id == datalist[index].sc_id);
-    if (item) {
-      item.count = item.count + 1;
-    } else {
-      var item = JSON.parse(JSON.stringify(datalist[index])); //just to make sure its not passing reference
-      item.count = 1;
-      addTOCart.push(item);
-    }
-  };
-
-  let decrementCount = (e) => {
-    if (counter <= 0) {
-      setCount(0);
-    } else {
-      setCount(counter - 1);
-    }
-    const mapping = { b11: 0, b12: 1, b13: 2, b14: 3, b15: 4, b16: 5 };
-    const a = e.target.id;
-    const index = mapping[a];
-    var item = addTOCart.find((x) => x.sc_id == datalist[index].sc_id);
-    if (item) {
-      if (item.count == 0) {
-        item.count = 0;
-      } else {
-        item.count = item.count - 1;
-      }
-    } else {
-      var item = JSON.parse(JSON.stringify(datalist[index])); //just to make sure its not passing reference
-      item.count = 1;
-      addTOCart.pop(item);
-    }
-  };
-
+  if (cat != catt) {
+    cat = catt;
+  }
+  console.log("9999 cat: ", cat);
   useEffect(() => {
     const fetchSubCategories = async () => {
       try {
         const res = await axios.get(
-          "http://localhost:8803/sub_categories/" + location.state.id
+          "http://localhost:8803/sub_categories/" + cat
         );
         datalist = res.data;
         document.getElementById("h1").innerHTML = res.data[0].sc_name;
@@ -79,25 +43,18 @@ const MainCategory = () => {
         document.getElementById("h3").innerHTML = res.data[2].sc_name;
         document.getElementById("h4").innerHTML = res.data[3].sc_name;
         document.getElementById("h5").innerHTML = res.data[4].sc_name;
-        document.getElementById("h6").innerHTML = res.data[5].sc_name;
+        // document.getElementById("h6").innerHTML = res.data[5].sc_name;
 
         document.getElementById("p1").innerHTML = res.data[0].sc_price;
         document.getElementById("p2").innerHTML = res.data[1].sc_price;
         document.getElementById("p3").innerHTML = res.data[2].sc_price;
         document.getElementById("p4").innerHTML = res.data[3].sc_price;
         document.getElementById("p5").innerHTML = res.data[4].sc_price;
-        document.getElementById("p6").innerHTML = res.data[5].sc_price;
+        // document.getElementById("p6").innerHTML = res.data[5].sc_price;
 
-        document.getElementById("img1").src = res.data[0].sc_image;
-        document.getElementById("img2").src = res.data[1].sc_image;
-        document.getElementById("img3").src = res.data[2].sc_image;
-        document.getElementById("img4").src = res.data[3].sc_image;
-        document.getElementById("img5").src = res.data[4].sc_image;
-        document.getElementById("img6").src = res.data[5].sc_image;
-
-        setTitle(location.state.id);
+        console.log("99999 data: ", res.data[0].sc_price);
       } catch (err) {
-        console.log("Error: ", err);
+        console.log("99999 Error: ", err);
       }
     };
     fetchSubCategories();
@@ -106,21 +63,28 @@ const MainCategory = () => {
   return (
     <div>
       <section className="header">
+      <select onChange={refreshPage}>
+          <option value="Select">Select Category</option>
+          <option value="Vegetables">Vegetables</option>
+          <option value="Fruits">Fruits</option>
+          <option value="Meat">Meat</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Sweat">Sweat</option>
+          <option value="FrozenFood">FrozenFood</option>
+        </select>
         <a className="logo">
           <i className="fas fa-shopping-bag icon"></i>
           <h1 className="logoTitle">Achievers Grocery</h1>
         </a>
 
-
-        <form action={filterData} className="searchForm">
+        <form action="" className="searchForm">
           <input
             type="search"
             name="search"
             id="search"
             className="searchBox"
-            placeholder="Search by Name..."
+            placeholder="Search here..."
           />
-
           <label htmlFor="search" className="searchPointer">
             <i className="fas fa-search icon"></i>
           </label>
@@ -129,61 +93,50 @@ const MainCategory = () => {
           <i className="fas fa-bars icon"></i>
         </div>
       </section>
-
       <section className="navbar" id="navbar">
-        <div className="iconContainer">
-          <Link to="/Cart" onClick={sendData}>
 
+        <div className="iconContainer">
+          <Link
+            to="/Cart"
+            state={{
+              value: addTOCart,
+            }}
+          >
             <a className="iconLink" title="Shopping Cart">
               <i className="fa fas fa-shopping-cart icon"></i>
             </a>
           </Link>
+       
+          <Link to="/Cart" className="iconLink" >
+            <i className="fa fas fa-shopping-cart icon"></i>
+            </Link> 
         </div>
-      </section>
 
+      </section>
       <section id="product" className="product">
-        <h2 id="maintitle" className="sectionTitle"></h2>
+        <h2 className="sectionTitle"> Vegetables</h2>
         <div className="container">
           <div className="box">
+            <h3 className="discount">-17.5%</h3>
             <figure className="figure">
-              <img id="img1" alt="banner" className="img" />
+              <img src={image4} alt="banner" className="img" />
             </figure>
             <h2 id="h1" className="title"></h2>
-
 
             <div className="price">
               <span id="p1" className="present"></span>
               <span className="previous">$15.3</span>
             </div>
 
-            <div class="modal">
-              <div class="modal_content">
-                <span class="close">&times;</span>
-                <p>Item Added!!!</p>
-              </div>
-            </div>
-
-            <div style={{ display: "flex", fontSize: "40px" }}>
-              <button id="b11" onClick={decrementCount} className="btn">
-                -
-              </button>
-              <div className="price">
-                <span className="present">{counter}</span>
-              </div>
-              <button id="b1" onClick={incrementCount} className="btn">
-                +
-              </button>
-            </div>
-
-            {/* <button id="b1" type="button" className="btn" onClick={handleClick}>
+            <button id="b1" type="button" className="btn" onClick={handleClick}>
               Add to Cart
-            </button> */}
+            </button>
           </div>
 
-
           <div className="box">
+            <h3 className="discount">-17.5%</h3>
             <figure className="figure">
-              <img id="img2" alt="banner" className="img" />
+              <img src={image1} alt="banner" className="img" />
             </figure>
             <h2 id="h2" className="title"></h2>
             <div className="price">
@@ -191,27 +144,15 @@ const MainCategory = () => {
               <span className="previous">$15.3</span>
             </div>
 
-            <div style={{ display: "flex", fontSize: "40px" }}>
-              <button id="b12" onClick={decrementCount} className="btn">
-                -
-              </button>
-              <div className="price">
-                <span className="present">{counter}</span>
-              </div>
-              <button id="b2" onClick={incrementCount} className="btn">
-                +
-              </button>
-            </div>
-            {/* <button id="b2" type="button" className="btn" onClick={handleClick}>
-
+            <button id="b2" type="button" className="btn" onClick={handleClick}>
               Add To Cart
-            </button> */}
+            </button>
           </div>
 
           <div className="box">
+            <h3 className="discount">-18.5%</h3>
             <figure className="figure">
-              <img id="img3" alt="banner" className="img" />
-
+              <img src={image2} alt="banner" className="img" />
             </figure>
             <h2 id="h3" className="title"></h2>
             <div className="price">
@@ -219,171 +160,117 @@ const MainCategory = () => {
               <span className="previous">$15.3</span>
             </div>
 
-            <div style={{ display: "flex", fontSize: "40px" }}>
-              <button id="b13" onClick={decrementCount} className="btn">
-                -
-              </button>
-              <div className="price">
-                <span className="present">{counter}</span>
-              </div>
-              <button id="b3" onClick={incrementCount} className="btn">
-                +
-              </button>
-            </div>
-            {/* <button id="b3" type="button" className="btn" onClick={handleClick}>
-
+            <button id="b3" type="button" className="btn" onClick={handleClick}>
               Add To Cart
-            </button> */}
+            </button>
           </div>
 
           <div className="box">
+            <h3 className="discount">-13.5%</h3>
             <figure className="figure">
-              <img id="img4" alt="banner" className="img" />
+              <img src={image3} alt="banner" className="img" />
             </figure>
-            <h2 id="h4" className="title"></h2>
+            <h2 className="title">Apple</h2>
             <div className="price">
               <span id="p4" className="present"></span>
               <span className="previous">$15.3</span>
             </div>
 
-            <div style={{ display: "flex", fontSize: "40px" }}>
-              <button id="b14" onClick={decrementCount} className="btn">
-                -
-              </button>
-              <div className="price">
-                <span className="present">{counter}</span>
-              </div>
-              <button id="b4" onClick={incrementCount} className="btn">
-                +
-              </button>
-            </div>
-            {/* <button id="b4" type="button" className="btn" onClick={handleClick}>
-
+            <button id="b4" type="button" className="btn" onClick={handleClick}>
               Add To Cart
-            </button> */}
+            </button>
           </div>
 
           <div className="box">
+            <h3 className="discount">-10.5%</h3>
             <figure className="figure">
-              <img id="img5" alt="banner" className="img" />
+              <img src={image4} alt="banner" className="img" />
             </figure>
-            <h2 id="h5" className="title"></h2>
+            <h2 id="h4" className="title"></h2>
             <div className="price">
               <span id="p5" className="present"></span>
               <span className="previous">$15.3</span>
             </div>
 
-            <div style={{ display: "flex", fontSize: "40px" }}>
-              <button id="b15" onClick={decrementCount} className="btn">
-                -
-              </button>
-              <div className="price">
-                <span className="present">{counter}</span>
-              </div>
-              <button id="b5" onClick={incrementCount} className="btn">
-                +
-              </button>
-            </div>
-            {/* <button id="b5" type="button" className="btn" onClick={handleClick}>
-
-           
+            <button id="b5" type="button" className="btn" onClick={handleClick}>
               Add To Cart
-            </button> */}
+            </button>
           </div>
 
           <div className="box">
+            <h3 className="discount">-30.5%</h3>
             <figure className="figure">
-              <img id="img6" alt="banner" className="img" />
+              <img src={image5} alt="banner" className="img" />
             </figure>
-            <h2 id="h6" className="title"></h2>
+            <h2 id="h5" className="title"></h2>
             <div className="price">
               <span id="p6" className="present"></span>
               <span className="previous">$15.3</span>
             </div>
 
-            <div style={{ display: "flex", fontSize: "40px" }}>
-              <button
-                id="b16"
-                onClick={decrementCount}
-                className="btn"
-                style={{ fontSize: "15px" }}
-              >
-                -
-              </button>
-              <div className="price">
-                <span className="present">{counter}</span>
-              </div>
-              <button id="b6" onClick={incrementCount} className="btn">
-                +
-              </button>
-            </div>
-            {/* <button id="b6" type="button" className="btn" onClick={handleClick}> */}
-            {/* Add To Cart */}
-            {/* </button> */}
-
+            <button id="b6" type="button" className="btn" onClick={handleClick}>
+              Add To Cart
+            </button>
           </div>
         </div>
-
       </section>
     </div>
   );
 };
-
-//Add items to cart
+let refreshPage = (e) => {
+  var val = e.target.value;
+  if (val == "Vegetables") {
+    sessionStorage.setItem("cat", JSON.stringify("1"));
+    window.location.reload(false);
+    console.log("999, cat: ", cat);
+  }
+  if (val == "Fruits") {
+    sessionStorage.setItem("cat", JSON.stringify("2"));
+    window.location.reload(false);
+    console.log("999, cat: ", cat);
+  }
+  if (val == "Meat") {
+    sessionStorage.setItem("cat", JSON.stringify("3"));
+    window.location.reload(false);
+    console.log("999, cat: ", cat);
+  }
+  if (val == "Dairy") {
+    sessionStorage.setItem("cat", JSON.stringify("4"));
+    window.location.reload(false);
+    console.log("999, cat: ", cat);
+  }
+  if (val == "Sweat") {
+    sessionStorage.setItem("cat", JSON.stringify("5"));
+    window.location.reload(false);
+    console.log("999, cat: ", cat);
+  }
+  if (val == "FrozenFood") {
+    sessionStorage.setItem("cat", JSON.stringify("6"));
+    window.location.reload(false);
+    console.log("999, cat: ", cat);
+  }
+};
 const handleClick = (e) => {
-  //jquery for showing popup
-  const modal = document.querySelector(".modal");
-  const closeBtn = document.querySelector(".close");
-  modal.style.display = "block";
-  closeBtn.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
-
-  const mapping = { b1: 0, b2: 1, b3: 2, b4: 3, b5: 4, b6: 5 };
+  console.log(e.target.id, "999 : ", datalist[1]);
   const a = e.target.id;
-  const index = mapping[a];
-  console.log("INDEX == ", index);
-  var item = addTOCart.find((x) => x.sc_id == datalist[index].sc_id);
-  if (item) {
-    item.count = item.count + 1;
-  } else {
-    var item = JSON.parse(JSON.stringify(datalist[index])); //just to make sure its not passing reference
-    item.count = 1;
-    addTOCart.push(item);
+  if (a == "b1") {
+    addTOCart.push(datalist[0]);
   }
-};
-//Fetch data added in cart and send it to backend
-let sendData = () => {
-  sessionStorage.setItem("items", JSON.stringify(addTOCart));
-};
-
-//Set Main title of Main Category page on the basis of c_id coming from category page
-let setTitle = (id) => {
-  if (id == 1) {
-    document.getElementById("maintitle").innerHTML = "VEGETABLES";
+  if (a == "b2") {
+    addTOCart.push(datalist[1]);
   }
-  if (id == 2) {
-    document.getElementById("maintitle").innerHTML = "FRUITS";
+  if (a == "b3") {
+    addTOCart.push(datalist[2]);
   }
-  if (id == 3) {
-    document.getElementById("maintitle").innerHTML = "MEAT";
+  if (a == "b4") {
+    addTOCart.push(datalist[3]);
   }
-  if (id == 4) {
-    document.getElementById("maintitle").innerHTML = "DAIRY";
+  if (a == "b5") {
+    addTOCart.push(datalist[4]);
   }
-  if (id == 5) {
-    document.getElementById("maintitle").innerHTML = "SWEETS";
+  if (a == "b6") {
+    addTOCart.push(datalist[5]);
   }
-  if (id == 6) {
-    document.getElementById("maintitle").innerHTML = "FROZEFOOD";
-  }
-};
-
-let filterData = () => {
-  const filteredSuggestions = datalist.filter((datalist) =>
-    datalist.sc_name.toString().toLowerCase()
-  );
-  return filteredSuggestions;
 };
 
 export default MainCategory;
