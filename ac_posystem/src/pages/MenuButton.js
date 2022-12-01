f81ccbcd1b65593da14a70cb831905a994f5a5fd
Madhavareddymukku <115957057+Madhavareddymukku@users.noreply.github.com>
2022-12-01 11:51:33 -0600

    Fixed issues with menu button functionality

--- ac_posystem/src/pages/MenuButton.js
+++ ac_posystem/src/pages/MenuButton.js
@@ -1,32 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
 class Menu extends React.Component {
-    constructor() {
-      super();
-      this.handleChange = this.handleChange.bind(this);
-      this.state = {
-        selectValue:'Go To'
-      };
-    }
-    handleChange(event){
-      this.setState({selectValue:event.target.value});
-      <Link
-        to="/MainCategory"
-        state={{
-        id: "1",
-        }}
-    >
-        event.target.value
-    </Link>
-    }
-      
-    render() {
-      return (
-              
-      );
-    }
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      selectValue:'Home'
+    };
   }
   
-  export default Menu;
+  handleChange(event){
+    this.setState({selectValue:event.target.value});
+  }
+    
+  render() {
+    return (
+      <div>
+        <select 
+          value={this.state.selectValue} 
+          onChange={this.handleChange} 
+        >
+          <option value="Home">Home</option>
+          <option value="Fruits">Fruits</option>
+          <option value="Vegetables">Vegetables</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Other">Other</option>
+        </select>
+      </div>        
+    );
+  }
+}
 
-
+  
+export default Menu;
