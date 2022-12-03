5b4317e39c1535d1cbdc7cf68450ccddad13d9cc
“AkhilPJ12” <akhilpj12@xn--gmail-hw3b.com>
2022-12-02 22:52:25 -0600

    Adding Test Cases

--- ac_posystem/backend/test-backend/test.js
+++ ac_posystem/backend/test-backend/test.js
@@ -28,20 +28,56 @@ test("if service instance is created", () => {
   expect(subCatvalue().length).toEqual(6);
 });
 
-// test("GET /categories", async () => {
-//   const res = await request(app).get("/categories");
-//   expect(res.statusCode).toBe(200);
-//   expect(res.body.length).toBeGreaterThan(0);
-// });
+test("GET /categories", async () => {
+  const res = await request(app).get("/categories");
+  expect(res.statusCode).toBe(200);
+  expect(res.body.length).toBeGreaterThan(0);
+});
 
-// test("GET /", async () => {
-//   const res = await request(app).get("/");
-//   expect(res.statusCode).toBe(200);
-//   expect(res.body.length).toBeGreaterThan(0);
-// });
+test("GET /", async () => {
+  const res = await request(app).get("/");
+  expect(res.statusCode).toBe(200);
+  expect(res.body.length).toBeGreaterThan(0);
+});
 
-// test("GET /sub_categories/:id", async () => {
-//   const res = await request(app).get("/sub_categories/1");
-//   expect(res.statusCode).toBe(200);
-//   expect(res.body.length).toBeGreaterThan(0);
-// });
+test("GET /sub_categories/:id", async () => {
+  const res = await request(app).get("/sub_categories/1");
+  expect(res.statusCode).toBe(200);
+  expect(res.body.length).toBeGreaterThan(0);
+});
+
+describe("Cart total", ()=>{
+
+  test("Checking cart", ()=>{
+      const cart = [
+          { cart: "Apple", price: 500 },
+          { cart: "Mango", price: 50 },
+          { cart: "Melon", price: 60 },
+          { cart: "Berry", price: 30 },
+          { cart: "Jack", price: 200 },
+      ]
+      // Calculations.getSum(cart);
+      expect(Calculations.getSum(cart)).toEqual(840);
+  })
+})
+
+describe("Cart total", ()=>{
+
+    test("Checking check", ()=>{
+        const cart = [
+            { cart: "Apple", price: 500 },
+            { cart: "Mango", price: 50 },
+            { cart: "Melon", price: 60 },
+            { cart: "Berry", price: 30 },
+            { cart: "Jack", price: 200 },
+        ]
+        expect(cart.length).toEqual(0);
+    })
+})
+
+test('render the title of an application', () => { 
+    test("Checking check", ()=>{
+    const titleEl = screen.getByText(/src/MainCategory);
+    expect(titleEl).toBeInTheDocument();
+    })
+  });
